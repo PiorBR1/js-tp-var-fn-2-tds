c6099cbdfa550c2580e1af8c2f1a8ab4879ffb8f
PiorBR1 <160504825+PiorBR1@users.noreply.github.com>
2024-11-22 08:11:49 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFluente = 3.3;
+const pontoFluenteSemZero = .5;
+
+const novaOperacao = primeiroNumero/ numeroPontoFluente;
+
+console.log (novaOperacao)
+
+//Nan 
+
+const alura = "Alura";
+console.log(alura*primeiroNumero);
+
+
